b10c575fa16154f39b6c16367703f0a6daf1e858
Charles Shih <shichen@Schen-Mac-Mini-2012.local>
2018-09-05 22:27:20 +0800

    Complete json2url() function.

--- ssr_utils.py
+++ ssr_utils.py
@@ -52,30 +52,55 @@ def url2json(url=''):
 
 
 def json2url(jdata={}):
-    # Check keywords and compose the paintext
+    """Convert the JSON to URL."""
+    # Check the SSR keywords
     for keyword in ('server', 'port', 'protocol', 'method', 'obfs', 'password'):
         if keyword not in jdata:
-            print '%s is missing in the json code.' % keyword
+            print 'keyword "%s" is missing in the json block.' % keyword
             return None
 
-    text = ':'.join(
+    # Join SSR keywords as paintext
+    text_kw = ':'.join(
         map(lambda x: jdata[x], ('server', 'port', 'protocol', 'method', 'obfs')))
-    text = text + ':' + _base64encode(jdata['password'])
+    text_kw = text_kw + ':' + _base64encode(jdata['password'])
 
-    print text
-    exit()
+    # Join additional parameters as paintext
+    text_pm = ''
+    for key in ('protoparam', 'obfsparam', 'remarks', 'group'):
+        if key in jdata:
+            text_pm = text_pm + '&' + key + '=' + _base64encode(jdata[key])
+        else:
+            text_pm = text_pm + '&' + key + '='
+    for key in ('udpport', 'uot'):
+        if key in jdata:
+            text_pm = text_pm + '&' + key + '=' + jdata[key]
+        else:
+            text_pm = text_pm + '&' + key + '=0'
+    text_pm = text_pm.lstrip('&')
 
-    for param in ('protoparam', 'obfsparam', 'remarks', 'group', 'udpport', 'uot'):
-    pass
+    # Combine as the paintext
+    text = text_kw + '/?' + text_pm
+
+    # Encoding as payload
+    payload = _base64encode(text)
+
+    # Compose the URL
+    url = 'ssr://' + payload
+
+    return(url)
 
 
+# ssr://server:port:protocol:method:obfs:password_base64/?params_base64
+# params_base64:
+# obfsparam=obfsparam_base64&protoparam=protoparam_base64&remarks=remarks_base64&group=group_base64&udpport=udpport&uot=uot[&...]
 
-
-
+url = 'ssr://c3NyLmV4YW1wbGUuY29tOjg4ODg6YXV0aF9jaGFpbl9hOmNoYWNoYTIwOnRsczEuMl90aWNrZXRfYXV0aDpiWGx3WVhOemQyOXlaQS8_cHJvdG9wYXJhbT0mb2Jmc3BhcmFtPSZyZW1hcmtzPVRWbFRVMUkmZ3JvdXA9NVp1OTZabUY1WXFnNllDZjU3cV82TGV2JnVkcHBvcnQ9MCZ1b3Q9MA'
+jdata = {'udpport': '0', 'protocol': 'auth_chain_a', 'uot': '0', 'server': 'ssr.example.com', 'port': '8888', 'obfs': 'tls1.2_ticket_auth', 'protoparam': '',
+         'remarks': 'MYSSR', 'obfsparam': '', 'password': 'mypassword', 'method': 'chacha20', 'group': '\xe5\x9b\xbd\xe9\x99\x85\xe5\x8a\xa0\xe9\x80\x9f\xe7\xba\xbf\xe8\xb7\xaf'}
 
 if __name__ == '__main__':
 
     j = url2json(url)
     u = json2url(j)
-    print u
+    print u == json2url(url2json(u))
     exit(0)
